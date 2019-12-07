1dca4f46e6da6e1b03e8243b2f6cc305c89fa583
Thomas Cancino <thomas.cancino2014@gmail.com>
2019-12-07 17:52:27 -0300

    Implementacion de Admin
    El admin es :nombre Troll
    pass Trolls123
    mail thomas.cancino@gmail.com

--- ArquitecturaDeSoftware/Apps/TrollBook/admin.py
+++ ArquitecturaDeSoftware/Apps/TrollBook/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from ArquitecturaDeSoftware.Apps.TrollBook.models import *
 # Register your models here.
+admin.site.register(Troll)
--- ArquitecturaDeSoftware/Apps/TrollBook/models.py
+++ ArquitecturaDeSoftware/Apps/TrollBook/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+#python manage.py createsuperuser
+class Troll(models.Model):
+    AppellidoPaterno= models.CharField(max_length=35)
+    AppellidoMaterno= models.CharField(max_length=35)
+    Nombres= models.CharField(max_length=35)
+    RUT= models.CharField(max_length=8)
+    FechaNacimiento = models.DateField()
+    SEXOS=(('F','Femenino'),('M','Masculino'))
+    Sexo=models.CharField(max_length=1,choices=SEXOS,default='M')
+
+    def NombreCompleto(self):
+        cadena= "{0} {1},{2}"
+        return cadena.format(self.AppellidoPaterno,self.AppellidoMaterno,self.Nombres)
+    
+    def __str__(self):
+        return self.NombreCompleto()

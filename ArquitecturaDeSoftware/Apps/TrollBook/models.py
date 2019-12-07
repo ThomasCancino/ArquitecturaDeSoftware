568de4a5fd97b2a8ada9eb32aff79a77af7f6f35
Thomas Cancino <thomas.cancino2014@gmail.com>
2019-12-07 18:10:34 -0300

    Agragacion base de datos

--- ArquitecturaDeSoftware/Apps/TrollBook/models.py
+++ ArquitecturaDeSoftware/Apps/TrollBook/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 #python manage.py createsuperuser
 class Troll(models.Model):
-    AppellidoPaterno= models.CharField(max_length=35)
-    AppellidoMaterno= models.CharField(max_length=35)
+    ApellidoPaterno= models.CharField(max_length=35)
+    ApellidoMaterno= models.CharField(max_length=35)
     Nombres= models.CharField(max_length=35)
     RUT= models.CharField(max_length=8)
     FechaNacimiento = models.DateField()
@@ -13,7 +13,7 @@ class Troll(models.Model):
 
     def NombreCompleto(self):
         cadena= "{0} {1},{2}"
-        return cadena.format(self.AppellidoPaterno,self.AppellidoMaterno,self.Nombres)
+        return cadena.format(self.ApellidoPaterno,self.ApellidoMaterno,self.Nombres)
     
     def __str__(self):
         return self.NombreCompleto()
